e33c0dda5b989f8683bfca3295dfd2d912ac4676
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-12-06 02:54:25 +0600

    pdf creator function created

--- index.js
+++ index.js
@@ -7,12 +7,28 @@
  */
  
 // Dependencies.
+ const puppeteer = require('puppeteer');
  
- 
-// App object or Module scaffolding.
- 
 // main functions or objects.
+const createPdf = async ()=>{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Creating options which will be passed later
+    const options = {
+        path: 'pdf/web1.pdf',
+        format: 'A4'
+    }
+
+    // Define which site's page will be captured
+    await page.goto('https://github.com/mohammad-mesbaul-haque', {waitUntil: 'networkidle2'});
+    await page.pdf(options);
+
+    await browser.close();
+} 
  
+// Call the main  function
+createPdf();
  
  
  
