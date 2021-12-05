1666c32995169b98f24f173f3b566ddc22c2964a
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-12-06 02:42:12 +0600

    index file initiated

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Pdf Creator Using Node js
+ * Description: Pdf Creator Using Node js and Puppeteer
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mohammad-mesbaul-haque
+ * Date: 06/12/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
